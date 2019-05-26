9ad4d58506050703f267de7e8f5697ecc0de6844
Samuel C <schalela@live.com>
2019-05-26 22:14:11 +1000

    reduce cache time to 1s

--- ssr/pages/index.js
+++ ssr/pages/index.js
@@ -7,7 +7,7 @@ const Index = () => {
 
 Index.getInitialProps = async ({ res }) => {
   if (res) {
-    res.setHeader('Cache-Control', 's-maxage=5, stale-while-revalidate');
+    res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate');
   }
 };
 
