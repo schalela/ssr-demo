1b94e3f4c1b5ba5758716cbb8ad8fe46d29bd883
Samuel C <schalela@live.com>
2019-06-13 00:40:30 +1000

    Added cache back

--- ssr/pages/index.js
+++ ssr/pages/index.js
@@ -4,9 +4,9 @@ import { Fixtures } from '../components';
 const Index = () => <Fixtures />;
 
 Index.getInitialProps = async ({ res }) => {
-  // if (res) {
-  //   res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate');
-  // }
+  if (res) {
+    res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate');
+  }
 };
 
 export default Index;
