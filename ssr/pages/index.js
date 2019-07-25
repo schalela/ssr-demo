22b6a0789ba11047c8968848afb44a68ddf35f6c
Samuel Chalela <samuel.chalela@thoughtworks.com>
2019-07-25 22:17:26 +1000

    Added SPR header

--- ssr/pages/index.js
+++ ssr/pages/index.js
@@ -3,4 +3,10 @@ import { Fixtures } from '../components';
 
 const Index = () => <Fixtures />;
 
+Index.getInitialProps = async ({ res }) => {
+  if (res) {
+    res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate');
+  }
+};
+
 export default Index;
