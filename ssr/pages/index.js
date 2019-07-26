2f5c73e43bc6a632b0d037efd5f28d8bba79eeae
Samuel Chalela <samuel.chalela@thoughtworks.com>
2019-07-27 01:05:59 +1000

    Remove SPR

--- ssr/pages/index.js
+++ ssr/pages/index.js
@@ -3,10 +3,4 @@ import { Fixtures } from '../components';
 
 const Index = () => <Fixtures />;
 
-Index.getInitialProps = async ({ res }) => {
-  if (res) {
-    res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate');
-  }
-};
-
 export default Index;
